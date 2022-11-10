198f956ef1a7617571fa0cab763d45db80e80231
TheoDorus <theodorusadiyatma96@gmail.com>
2022-11-10 21:16:00 +0700

    Initial commit

---
+++ GD10_C_10692/app/src/main/java/com/example/gd10_c_10692/ResponseCreate.kt
@@ -0,0 +1,9 @@
+package com.example.gd10_c_10692
+
+import com.google.gson.annotations.SerializedName
+
+class ResponseCreate (
+    @SerializedName("status") val stt:Int,
+    @SerializedName("error") val e:Boolean,
+    @SerializedName("message") val pesan:String,
+)
